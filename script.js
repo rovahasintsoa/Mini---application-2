9e144dafa8cd2b3d477609b30e70dfb4e2896480
rovahasintsoa <63501157+rovahasintsoa@users.noreply.github.com>
2020-08-04 14:48:14 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
-
 const solution = Math.floor(Math.random()*11)  // chiffre au hasard qu'on va designé
-var count = 0;  // nombre de click sur le boutton
+var count = 0;  // nombre de click sur le bouton
+
 function function_add() {
 var test = document.getElementById('test').value;  // recuperation du chiffre
 document.getElementById("myDiv").innerText += " " + test + "," ;
-if (count < 3) {
   if (solution != test) {
     document.getElementById('faux').innerText = "Faux";
   } else {
     document.getElementById('faux').innerText = "Félicitation! la solution est vraiment: " + test;
   }
-} else {
-  document.getElementById('faux').innerText ="Dommage! vous avez perdu, la vraie reponse est: " + solution;
-  document.getElementById('boutton').disabled = true;
-}
-
+  console.log(count)
+  if (count > 1) {
+    document.getElementById('btn').disabled = false;
+    count = 0;    // remise à 0 le compteur pour que le bouton "affichez solution" devient grisé apres actualisation 
+  } else {
+    document.getElementById('btn').disabled = true;
+  }
 count++;
-
+}
+console.log(count);
+function afficheSolution() {
+  document.getElementById('sltion').innerText = "La solution est: " + solution ;
 }
